f99bbddb40c0de4eb2c7b80f6a817c55990f49f1
Matt Westcott <mattwestcott@users.noreply.github.com>
2022-02-22 12:16:26 +0900

    Tidied up project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   env: {
+    es6: true,
     browser: true,
     es2021: true,
   },
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {},
 };
